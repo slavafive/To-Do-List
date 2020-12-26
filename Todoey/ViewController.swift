0f071e55beaf60ce2994788d1cc63d76fc3c66cf
Vyacheslav Efimov <efimov@element-soft.com>
2020-12-26 16:34:52 +0300

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Вячеслав Ефимов on 26.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
